d217d02f449d5dbd98c22c58f70b41c5a0d7e147
daleleo <daleleo@yandex.ru>
2022-12-07 01:14:31 +1000

    new file:   taco-cloud/.gitignore 	new file:   taco-cloud/.mvn/wrapper/maven-wrapper.jar 	new file:   taco-cloud/.mvn/wrapper/maven-wrapper.properties 	new file:   taco-cloud/mvnw 	new file:   taco-cloud/mvnw.cmd 	new file:   taco-cloud/pom.xml 	new file:   taco-cloud/src/main/java/META-INF/MANIFEST.MF 	new file:   taco-cloud/src/main/java/project/spring/TacoCloudApplication.java 	new file:   taco-cloud/src/main/java/project/spring/tacos/HomeCotroller.java 	new file:   taco-cloud/src/main/resources/application.properties 	new file:   taco-cloud/src/main/resources/templates/home.html 	new file:   taco-cloud/src/test/java/project/spring/TacoCloudApplicationTests.java

---
+++ taco-cloud/src/main/java/project/spring/tacos/HomeCotroller.java
@@ -0,0 +1,14 @@
+package project.spring.tacos;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeCotroller {
+
+	@GetMapping("/")
+	public String home() {
+		return "home";
+	}
+
+}
